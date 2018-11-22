7678da198a5080288033957881ac162d9158c357
Fedor Borshev <f@f213.in>
2018-11-22 10:45:07 +0300

    Ready for packaging in private env

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from distutils.core import setup
 
 setup(
-    name='django-auditlog',
-    version='0.4.5',
+    name='django-auditlog-compat',
+    version='0.4.6',
     packages=['auditlog', 'auditlog.migrations', 'auditlog.management', 'auditlog.management.commands'],
     package_dir={'': 'src'},
     url='https://github.com/jjkester/django-auditlog',
     license='MIT',
     author='Jan-Jelle Kester',
-    description='Audit log app for Django',
+    description='Audit log app for Django (local fork with compatible jsonfield)',
     install_requires=[
         'django-jsonfield>=1.0.0',
+        'django-jsonfield-compat==0.4.4',
         'python-dateutil==2.6.0'
     ],
     zip_safe=False,
@@ -22,5 +23,5 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'License :: OSI Approved :: MIT License',
-    ],        
+    ],
 )
