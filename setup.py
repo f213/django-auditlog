5b6c06994ae1fdcec58abcafa90317ec00a77dc5
Fedor Borshev <fedor@borshev.com>
2020-05-02 13:47:16 +0300

    Version bump

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
     name='django-auditlog-compat',
-    version='0.4.6',
+    version='0.5.0',
     packages=['auditlog', 'auditlog.migrations', 'auditlog.management', 'auditlog.management.commands'],
     package_dir={'': 'src'},
-    url='https://github.com/jjkester/django-auditlog',
+    url='https://github.com/f213/django-auditlog-compat',
     license='MIT',
     author='Jan-Jelle Kester',
     description='Audit log app for Django (local fork with compatible jsonfield)',
@@ -16,12 +16,10 @@ setup(
     ],
     zip_safe=False,
     classifiers=[
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: MIT License',
     ],
 )
