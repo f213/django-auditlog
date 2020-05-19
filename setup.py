9ed9703c9e23fccbabd1e7100bfbe244059b466b
Fedor Borshev <fedor@borshev.com>
2020-05-19 21:47:48 +0300

    Updated django-jsonfield-compat dep to my custom version with django 3.0 support

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-auditlog-compat',
-    version='0.5.2',
+    version='0.5.3',
     packages=['auditlog', 'auditlog.migrations', 'auditlog.management', 'auditlog.management.commands'],
     package_dir={'': 'src'},
     url='https://github.com/f213/django-auditlog-compat',
@@ -11,8 +11,8 @@ setup(
     description='Audit log app for Django (local fork with compatible jsonfield)',
     install_requires=[
         'django-jsonfield==1.4.0',
-        'django-jsonfield-compat==0.4.4',
-        'python-dateutil>=2.8.0'
+        'python-dateutil>=2.8.0',
+        'django-jsonfield-compat@git+https://github.com/f213/django-jsonfield-compat.git@b1f1955077041fe9f14eb32be7bbcc6a7525e366',
     ],
     zip_safe=False,
     classifiers=[
